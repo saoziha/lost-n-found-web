f86ffcb4778db368e1b311c21557eb60010df069
ngxuanthang1111 <ngxuanthang1111@gmail.com>
2019-10-09 12:57:17 +0700

    add link tag font & style

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -1,57 +1,10 @@
-// import React, {Component} from 'react';
-//
-// class Dashboard extends Component {
-//   render() {
-//     return (
-//       <div>
-//
-//       </div>
-//     );
-//   }
-// }
-//
-// export default Dashboard;
-/*!
-
-=========================================================
-* Black Dashboard React v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, {Fragment} from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react plugin used to create charts
 // reactstrap components
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Col,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  FormGroup,
-  Input,
-  Label,
-  Row,
-  Table,
-  UncontrolledDropdown,
-  UncontrolledTooltip
-} from "reactstrap";
+import {Button, Card, CardHeader, CardTitle, Col, FormGroup, Input, Label, Row} from "reactstrap";
+import LostTable from "../Component/Table/LostTable";
 
 // core components
 
@@ -59,7 +12,8 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bigChartData: "data1"
+      bigChartData: "data1",
+      sltCity: 0
     };
   }
 
@@ -69,9 +23,18 @@ class Dashboard extends React.Component {
     });
   };
 
+  onHandleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
-      <>
+      <Fragment>
         <div className="content">
           <Row>
             <Col xs="12">
@@ -79,196 +42,123 @@ class Dashboard extends React.Component {
                 <CardHeader>
                   <Row>
                     <Col className="text-left"
-                         sm="6">
-                      <h5 className="card-category">Total Shipments</h5>
-                      <CardTitle tag="h2">Performance</CardTitle>
+                         xs="12"
+                         md="8"
+                         lg="8"
+                         sm="8">
+                      <h5 className="card-category">
+                        What do you need?
+                      </h5>
+                      <CardTitle tag="h2">Latest News</CardTitle>
                     </Col>
-                    <Col sm="6">
-                      <ButtonGroup
-                        className="btn-group-toggle float-right"
-                        data-toggle="buttons"
-                      >
-                        <Button
-                          tag="label"
-                          className={classNames("btn-simple", {
-                            active: this.state.bigChartData === "data1"
-                          })}
-                          color="info"
-                          id="0"
-                          size="sm"
-                          onClick={() => this.setBgChartData("data1")}
-                        >
-                          <input
-                            defaultChecked
-                            className="d-none"
-                            name="options"
-                            type="radio"
-                          />
-                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
-                            Accounts
-                          </span>
-                          <span className="d-block d-sm-none">
-                            <i className="tim-icons icon-single-02"/>
-                          </span>
-                        </Button>
-                        <Button
-                          color="info"
-                          id="1"
-                          size="sm"
-                          tag="label"
-                          className={classNames("btn-simple", {
-                            active: this.state.bigChartData === "data2"
-                          })}
-                          onClick={() => this.setBgChartData("data2")}
-                        >
-                          <input
-                            className="d-none"
-                            name="options"
-                            type="radio"
-                          />
-                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
-                            Purchases
-                          </span>
-                          <span className="d-block d-sm-none">
-                            <i className="tim-icons icon-gift-2"/>
-                          </span>
-                        </Button>
-                        <Button
-                          color="info"
-                          id="2"
-                          size="sm"
-                          tag="label"
-                          className={classNames("btn-simple", {
-                            active: this.state.bigChartData === "data3"
-                          })}
-                          onClick={() => this.setBgChartData("data3")}
-                        >
-                          <input
-                            className="d-none"
-                            name="options"
-                            type="radio"
-                          />
-                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
-                            Sessions
-                          </span>
-                          <span className="d-block d-sm-none">
-                            <i className="tim-icons icon-tap-02"/>
-                          </span>
-                        </Button>
-                      </ButtonGroup>
+                    <Col className="text-left"
+                         xs="12"
+                         sm="4"
+                         md="4"
+                         lg="4"
+                    >
+                      <div className="form-row">
+                        <FormGroup className="col-md-12">
+                          <Label for="inputState">City</Label>
+                          <Input type="select"
+                                 name="sltCity"
+                                 id="inputState"
+                                 value={this.state.sltCity}
+                                 onChange={this.onHandleChange}
+                          >
+                            <option value={0}>Da Nang</option>
+                            <option value={1}>Ho Chi Minh</option>
+                            <option value={2}>Ha Noi</option>
+                            <option value={3}>Hai Phong</option>
+                          </Input>
+                        </FormGroup>
+                      </div>
                     </Col>
                   </Row>
                 </CardHeader>
-
               </Card>
             </Col>
           </Row>
           <Row>
-            <Col lg="4">
-              <Card className="card-chart">
-                <CardHeader>
-                  <h5 className="card-category">Total Lost</h5>
-                  <CardTitle tag="h3">
-                    <i className="tim-icons icon-bell-55 text-info"/>{" "}
-                    763,215
-                  </CardTitle>
-                </CardHeader>
+            <Col xs="12"
+                 lg="12"
+                 className="wrapped-button">
 
-              </Card>
-            </Col>
-            <Col lg="4">
-              <Card className="card-chart">
-                <CardHeader>
-                  <h5 className="card-category">Daily Sales</h5>
-                  <CardTitle tag="h3">
-                    <i className="tim-icons icon-delivery-fast text-primary"/>{" "}
-                    3,500€
-                  </CardTitle>
-                </CardHeader>
+              {/*Button Lost*/}
 
-              </Card>
-            </Col>
-            <Col lg="4">
-              <Card className="card-chart">
-                <CardHeader>
-                  <h5 className="card-category">Completed Tasks</h5>
+              <Col lg="6">
+                <Button
+                  tag="label"
+                  className={classNames("btn-simple", {
+                    active: this.state.bigChartData === "data1"
+                  })}
+                  color="info"
+                  id="0"
+                  size="md"
+                  onClick={() => this.setBgChartData("data1")}
+                >
+                  <input
+                    defaultChecked
+                    className="d-none"
+                    name="options"
+                    type="radio"
+                  />
+                  <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                <h5 className="card-categosry">Total Lost</h5>
+                    <CardTitle tag="h3">
+                      <i className="tim-icons icon-spaceship text-info"/>{" "}
+                      26.354
+                    </CardTitle>
+                          </span>
+                  <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-single-02"/>
+                          </span>
+                </Button>
+              </Col>
+
+              {/*Button Found*/}
+              <Col lg="6">
+                <Button
+                  color="info"
+                  id="1"
+                  size="md"
+                  tag="label"
+                  className={classNames("btn-simple", {
+                    active: this.state.bigChartData === "data2"
+                  })}
+                  onClick={() => this.setBgChartData("data2")}
+                >
+                  <input
+                    className="d-none"
+                    name="options"
+                    type="radio"
+                  />
+                  <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+
+              <h5 className="card-category">Total Found</h5>
                   <CardTitle tag="h3">
-                    <i className="tim-icons icon-send text-success"/> 12,100K
+                    <i className="tim-icons icon-zoom-split text-primary"/>{" "}
+                    25.431
                   </CardTitle>
-                </CardHeader>
-              </Card>
+                          </span>
+                  <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-gift-2"/>
+                          </span>
+                </Button>
+              </Col>
             </Col>
+
           </Row>
-          <Row>
 
+
+          <Row>
             <Col lg="12"
                  md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h4">Simple Table</CardTitle>
-                </CardHeader>
-                <CardBody>
-                  <Table className="tablesorter"
-                         responsive>
-                    <thead className="text-primary">
-                    <tr>
-                      <th>Name</th>
-                      <th>Country</th>
-                      <th>City</th>
-                      <th className="text-center">Salary</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                      <td>Dakota Rice</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
-                      <td className="text-center">$36,738</td>
-                    </tr>
-                    <tr>
-                      <td>Minerva Hooper</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
-                      <td className="text-center">$23,789</td>
-                    </tr>
-                    <tr>
-                      <td>Sage Rodriguez</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
-                      <td className="text-center">$56,142</td>
-                    </tr>
-                    <tr>
-                      <td>Philip Chaney</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
-                      <td className="text-center">$38,735</td>
-                    </tr>
-                    <tr>
-                      <td>Doris Greene</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                      <td className="text-center">$63,542</td>
-                    </tr>
-                    <tr>
-                      <td>Mason Porter</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
-                      <td className="text-center">$78,615</td>
-                    </tr>
-                    <tr>
-                      <td>Jon Porter</td>
-                      <td>Portugal</td>
-                      <td>Gloucester</td>
-                      <td className="text-center">$98,615</td>
-                    </tr>
-                    </tbody>
-                  </Table>
-                </CardBody>
-              </Card>
+              <LostTable/>
             </Col>
           </Row>
         </div>
-      </>
+      </Fragment>
     );
   }
 }
