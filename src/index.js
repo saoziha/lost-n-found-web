1d6427bfdada6833a77ca49dbf7fddf993679b7f
ngxuanthang1111 <ngxuanthang1111@gmail.com>
2019-10-09 11:55:41 +0700

    add more route for routes.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {Router ,Route, Switch, Redirect} from 'react-router-dom';
-import { createBrowserHistory } from "history";
+import {Redirect, Route, Router, Switch} from 'react-router-dom';
+import {createBrowserHistory} from "history";
 // import { Provider } from 'react-redux';
 import AdminPage from "./Layouts/Admin/AdminPage";
 import "./assets/scss/black-dashboard-react.scss";
 import "./assets/css/nucleo-icons.css";
+
 const history = createBrowserHistory();
-{/*<Provider store = {store}></Provider>*/}
+{/*<Provider store = {store}></Provider>*/
+}
 
 ReactDOM.render(
-  <Router history = {history}>
+  <Router history={history}>
     <Switch>
       {/*<App />*/}
-      <Route path="/admin" render={props => <AdminPage {...props}/>} />
-      <Redirect from='/' to='/admin/dashboard'  render={props => <AdminPage {...props}/>} />
+      <Route path="/admin"
+             render={props => <AdminPage {...props}/>}/>
+      <Redirect from="/"
+                to="/admin/dashboard"
+                render={props => <AdminPage {...props}/>}/>
     </Switch>
   </Router>
 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,32 @@
-import Dashboard from './Views/Dashboard'
 import React from 'react';
+import Dashboard from './Views/Dashboard'
+import Post from './Views/Post'
+import UserProfile from './Views/UserProfile'
 
 
 let routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    icon: 'tim-icons icon-chart-pie-36',
+    icon: 'tim-icons icon-notes',
     component: () => <Dashboard />,
     layout: '/admin'
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    icon: 'tim-icons icon-send',
+    component : ()=> <Post />,
+    layout: '/admin'
+  },
+  {
+    path: '/user-profile',
+    name: 'User Profile',
+    icon: 'tim-icons icon-single-02',
+    component : ()=> <UserProfile/>,
+    layout: '/admin'
   }
+
 ];
 
 export default routes;
